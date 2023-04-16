4abb55fa669d05d987886e438d8ba8d0c6e896f0
Soorya Narayanan DR <sooryanarayanandr2002@gmail.com>
2023-04-16 13:22:02 +0530

    Create PalindromeBinary.java

---
+++ PalindromeBinary.java
@@ -0,0 +1,24 @@
+public class PalindromeBinary {
+    public static boolean isPalindromeBinary(int n) {
+        String binary = Integer.toBinaryString(n);
+        int length = binary.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (binary.charAt(i) != binary.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        int n = 10;
+        boolean isPalindrome = isPalindromeBinary(n);
+        System.out.println("Binary representation of " + n + " is " + Integer.toBinaryString(n));
+        if (isPalindrome) {
+            System.out.println("Binary representation of " + n + " is a palindrome.");
+        } else {
+            System.out.println("Binary representation of " + n + " is not a palindrome.");
+        }
+    }
+}
+
